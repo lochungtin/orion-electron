a24ca75f949ab30ea36c677bf839a66a90d4813e
lochungtin <lochungtin@gmail.com>
2021-03-13 23:46:44 +0800

    loading screen

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import AppNav from './navigation';
 
 import './App.css';
 import './css/device.css';
+import './css/loading.css';
 import './css/login.css';
 import './css/main.css';
 import './css/nav.css';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default class App extends React.Component {
 
--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 
 import NavBar from '../components/NavBar';
 
@@ -7,6 +8,22 @@ class Screen extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            loading: true,
+            progress: 0,
+            prompt: 'prompt text'
+        }
+
+        this.loading(1);
+    }
+
+    loading = val => {
+        const progress = (val + 1) % 100;
+        if (this.state.loading) {
+            this.setState({ progress });
+            setTimeout(() => this.loading(progress), 100);
+        }
     }
 
     render() {
@@ -18,7 +35,28 @@ class Screen extends React.Component {
                     </p>
                 </NavBar>
                 <div className='content'>
-                    
+                    <div className='loadingProgressContainer'>
+                        <CircularProgressbar
+                            className='loadingProgressCircle'
+                            value={this.state.progress}
+                            strokeWidth={1}
+                            styles={{
+                                path: {
+                                    stroke: '#e6b329',
+                                    strokeLinecap: 'round',
+                                },
+                                trail: {
+                                    stroke: '#1e1e1e',
+                                },
+                            }}
+                        />
+                        <p className='loadingProgressText'>
+                            {'calculating ...'}
+                        </p>
+                        <p className='loadingText'>
+                            {this.state.prompt}
+                        </p>
+                    </div>
                 </div>
             </>
         );
